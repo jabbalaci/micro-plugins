4a6964eccb3104920f540d99e0ea7b2f64a1cbad
Jabba Laci <jabba.laci@gmail.com>
2023-11-14 15:27:39 +0100

    replace is refactored

--- replace/replace.lua
+++ replace/replace.lua
@@ -1,4 +1,4 @@
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 -- written by Laszlo Szathmary (jabba.laci@gmail.com), 2023
 -- https://github.com/jabbalaci/micro-plugins
@@ -6,7 +6,7 @@ VERSION = "0.1.0"
 local micro = import("micro")
 
 --global variable, false by default
-IsOverwriteMode = false
+local IsOverwriteMode = false
 
 function Replace(bp)
     IsOverwriteMode = not IsOverwriteMode
@@ -14,5 +14,5 @@ function Replace(bp)
     if IsOverwriteMode then
         msg = "REPLACE"
     end
-	micro.InfoBar():Message(msg)
+    micro.InfoBar():Message(msg)
 end
