423171048225d803ecdd65cc3bd11878647f27b0
Jabba Laci <jabba.laci@gmail.com>
2024-12-24 12:13:03 +0100

    Issue https://github.com/jabbalaci/micro-plugins/issues/2

--- replace/replace.lua
+++ replace/replace.lua
@@ -1,9 +1,10 @@
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
--- written by Laszlo Szathmary (jabba.laci@gmail.com), 2023
+-- written by Laszlo Szathmary (jabba.laci@gmail.com), 2023--2024
 -- https://github.com/jabbalaci/micro-plugins
 
 local micro = import("micro")
+local config = import("micro/config")
 
 --global variable, false by default
 local IsOverwriteMode = false
@@ -15,4 +16,19 @@ function Replace(bp)
         msg = "REPLACE"
     end
     micro.InfoBar():Message(msg)
+end
+
+function onEscape(bp)
+    local msg = ""
+    if IsOverwriteMode then
+        msg = "REPLACE (Normal mode: Insert)"
+    end
+    micro.InfoBar():Message(msg)
+    -- Return false to allow the ESC key to perform its normal function
+    return false
+end
+
+function init()
+    -- Bind the ESC key to our onEscape function
+    config.TryBindKey("Escape", "lua:replace.onEscape", false)
 end
